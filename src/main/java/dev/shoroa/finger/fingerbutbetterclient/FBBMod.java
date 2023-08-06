eeece67da9410eeb14040f9394fd7bb87a3cbcf4
Shoroa <adamko.mc@gmail.com>
2023-08-06 05:25:51 +0200

    Initial commit

---
+++ src/main/java/dev/shoroa/finger/fingerbutbetterclient/FBBMod.java
@@ -0,0 +1,10 @@
+package dev.shoroa.finger.fingerbutbetterclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FBBMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/dev/shoroa/finger/fingerbutbetterclient/client/FBBClient.java
@@ -0,0 +1,10 @@
+package dev.shoroa.finger.fingerbutbetterclient.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FBBClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
